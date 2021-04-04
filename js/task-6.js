809b1f100ead635b0dbdb86b96ac81e2ebd63605
Dmitry Karas <dmitrythekaras@gmail.com>
2021-04-04 12:20:38 +0300

    task-6 condition fix

--- js/task-6.js
+++ js/task-6.js
@@ -7,16 +7,16 @@ const inputEl = document.querySelector('#validation-input');
 
 function onInputTyping() {
   const inputTextLength = inputEl.value.length;
-  const maxLength = inputEl.getAttribute('data-length');
+  const dataLength = Number(inputEl.getAttribute('data-length'));
 
-  if (inputTextLength <= maxLength) {
-    inputEl.classList.add('valid');
-    inputEl.classList.remove('invalid');
-  }
-
-  if (inputTextLength > maxLength) {
+  if (inputTextLength > dataLength || inputTextLength < dataLength) {
     inputEl.classList.add('invalid');
     inputEl.classList.remove('valid');
+  }
+
+  if (inputTextLength === dataLength) {
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
   }
 
   if (!inputTextLength) {
